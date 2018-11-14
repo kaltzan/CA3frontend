723edaa2efe0a1aa4a257ba3171d31d36b88484b
Ionsight <ionsight@users.noreply.github.com>
2018-11-14 12:53:08 +0100

    styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
 import React, { Component } from "react"
 import facade from "./apiFacade";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loggedIn: false }
+  }
+  logout = () => {
+    facade.logout();
+    this.setState({ loggedIn: false });
+  }
+  login = (user, pass) => {
+    facade.login(user, pass)
+      .then(res => this.setState({ loggedIn: true }));
+  }
+  render() {
+    return (
+      <div>
+        {!this.state.loggedIn ? (<LogIn login={this.login} />) :
+          (<div>
+            <LoggedIn />
+            <button onClick={this.logout}>Logout</button>
+          </div>)}
+      </div>
+    )
+  }
+}
+
 class LogIn extends Component {
   constructor(props) {
     super(props);
@@ -15,6 +44,7 @@ class LogIn extends Component {
   render() {
     return (
       <div>
+        <Menu />
         <h2>Login</h2>
         <form onSubmit={this.login} onChange={this.onChange} >
           <input placeholder="User Name" id="username" />
@@ -42,29 +72,85 @@ class LoggedIn extends Component {
     )
   }
 }
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { loggedIn: false }
-  }
-  logout = () => {
-    facade.logout();
-    this.setState({ loggedIn: false });
-  }
-  login = (user, pass) => {
-    facade.login(user, pass)
-      .then(res => this.setState({ loggedIn: true }));
-  }
-  render() {
-    return (
-      <div>
-        {!this.state.loggedIn ? (<LogIn login={this.login} />) :
-          (<div>
-            <LoggedIn />
-            <button onClick={this.logout}>Logout</button>
-          </div>)}
-      </div>
-    )
-  }
+
+
+
+function Menu() {
+  return (
+    <Router>
+      <Switch>
+        <ul className="header">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/topics">Topics</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/topics" component={Topics} />
+      </Switch>
+    </Router>
+  );
 }
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Topics({ match }) {
+  return (
+    <div>
+      <h2>Topics</h2>
+      <ul>
+        <li>
+          <Link to={`${match.url}/rendering`}>Rendering with React</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/components`}>Components</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+        </li>
+      </ul>
+
+      <Route path={`${match.path}/:topicId`} component={Topic} />
+      <Route
+        exact
+        path={match.path}
+        render={() => <h3>Please select a topic.</h3>}
+      />
+    </div>
+  );
+}
+
+function Topic({ match }) {
+  return (
+    <div>
+      <h3>{match.params.topicId}</h3>
+    </div>
+  );
+}
+
+
+
 export default App; 
