b0a25d33a8d5ab60e1c67cf1deaa8dcbe274d730
Steven <Steven-ptkhuu@hotmail.com>
2018-11-15 10:30:26 +0100

    made navigation(bar), routes and transfered login component to login.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react"
 import facade from "./apiFacade";
 import { HashRouter as Router, Route, NavLink as Link, Switch } from "react-router-dom";
+import Navigation from "./Navigation.js";
 
 
 class App extends Component {
@@ -44,7 +45,6 @@ class LogIn extends Component {
   render() {
     return (
       <div>
-        <Menu />
         <h2>Login</h2>
         <form onSubmit={this.login} onChange={this.onChange} >
           <input placeholder="User Name" id="username" />
@@ -74,83 +74,6 @@ class LoggedIn extends Component {
 }
 
 
-
-function Menu() {
-  return (
-    <Router>
-      <div>
-        <ul className="header">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/topics">Topics</Link>
-          </li>
-        </ul>
-
-        <hr />
-        <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/topics" component={Topics} />
-        </Switch>
-        </div>
-    </Router>
-  );
-}
-
-function Home() {
-  return (
-    <div>
-      <h2>Home</h2>
-    </div>
-  );
-}
-
-function About() {
-  return (
-    <div>
-      <h2>About</h2>
-    </div>
-  );
-}
-
-function Topics({ match }) {
-  return (
-    <div>
-      <h2>Topics</h2>
-      <ul>
-        <li>
-          <Link to={`${match.url}/rendering`}>Rendering with React</Link>
-        </li>
-        <li>
-          <Link to={`${match.url}/components`}>Components</Link>
-        </li>
-        <li>
-          <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-        </li>
-      </ul>
-
-      <Route path={`${match.path}/:topicId`} component={Topic} />
-      <Route
-        exact
-        path={match.path}
-        render={() => <h3>Please select a topic.</h3>}
-      />
-    </div>
-  );
-}
-
-function Topic({ match }) {
-  return (
-    <div>
-      <h3>{match.params.topicId}</h3>
-    </div>
-  );
-}
 
 
 
---
+++ src/Topics.js
@@ -0,0 +1,37 @@
+import React, {Component} from "react";
+import { HashRouter as Router, Route, NavLink as Link, Switch } from "react-router-dom";
+
+function Topics({ match }) {
+    return (
+      <div>
+        <h2>Topics</h2>
+        <ul>
+          <li>
+            <Link to={`${match.url}/rendering`}>Rendering with React</Link>
+          </li>
+          <li>
+            <Link to={`${match.url}/components`}>Components</Link>
+          </li>
+          <li>
+            <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+          </li>
+        </ul>
+  
+        <Route path={`${match.path}/:topicId`} component={Topic} />
+        <Route
+          exact
+          path={match.path}
+          render={() => <h3>Please select a topic.</h3>}
+        />
+      </div>
+    );
+  }
+  
+  function Topic({ match }) {
+    return (
+      <div>
+        <h3>{match.params.topicId}</h3>
+      </div>
+    );
+  }
+  export default Topics;
